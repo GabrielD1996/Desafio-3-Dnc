1d06504dad92535027c8d2a9ca2aa067a717c1e8
GabrielD1996 <gabrieldn1996@gmail.com>
2024-06-26 17:54:01 -0300

    Feitas melhorias na Navbar e adicionada funçao para rolagem suave entre setores

--- src/components/section/Navbar.jsx
+++ src/components/section/Navbar.jsx
@@ -5,21 +5,33 @@ import Nav from 'react-bootstrap/Nav'
 import { useState } from 'react'
 import Tech from '../elements/Tech'
 
-
+ 
 function Navbar() {
     const [showTechnologies, setShowTechnologies] = useState(false);
 
     const handleToggleTechnologies = () => {
         setShowTechnologies(!showTechnologies);
+       
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"   
+        });
+    };
+
+    const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
     };
 
     return (
         <><nav className={styles.navbar}>
-            <div className={styles.links}>
+            <div  className={styles.links}>
                 <ul>
-                    <li><Nav.Link href='#projects'>Projetos</Nav.Link></li>
-                    <li><Nav.Link href='#timeline'>Sobre mim</Nav.Link></li>
-                    <li><Nav.Link onClick={handleToggleTechnologies}>Tecnologias</Nav.Link></li>
+                    <li><Nav.Link  onClick={() => scrollToSection('projects')} >Projetos</Nav.Link></li>
+                    <li><Nav.Link onClick={() => scrollToSection('timeline')}>Sobre mim</Nav.Link></li>
+                    <li><Nav.Link href='#' onClick={handleToggleTechnologies}>Tecnologias</Nav.Link></li>
                 </ul>
             </div>
             <div className={styles.icons}>
